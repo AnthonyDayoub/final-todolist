21c93154f3cf19b9c27fe7c034aba20521477116
grigolir <grigolir@moravian.edu>
2026-04-26 23:28:11 -0400

    removed certain fonts

--- composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/csci215final/composeapp/generated/resources/Drawable0.commonMain.kt
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/csci215final/composeapp/generated/resources/Drawable0.commonMain.kt
@@ -3,16 +3,55 @@
 package csci215final.composeapp.generated.resources
 
 import kotlin.OptIn
-import org.jetbrains.compose.resources.InternalResourceApi
+import kotlin.String
+import kotlin.collections.MutableMap
 import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.InternalResourceApi
+import org.jetbrains.compose.resources.ResourceContentHash
 import org.jetbrains.compose.resources.ResourceItem
-import org.jetbrains.compose.resources.LanguageQualifier
-import org.jetbrains.compose.resources.RegionQualifier
-import org.jetbrains.compose.resources.ThemeQualifier
-import org.jetbrains.compose.resources.DensityQualifier
 
-internal val Res.drawable.checkbox: DrawableResource by lazy { DrawableResource("drawable:checkbox", setOf(ResourceItem(setOf(),"drawable/checkbox.png",-1,-1))) }
-internal val Res.drawable.checkmark: DrawableResource by lazy { DrawableResource("drawable:checkmark", setOf(ResourceItem(setOf(),"drawable/checkmark.png",-1,-1))) }
-internal val Res.drawable.checkmarkAppLogo: DrawableResource by lazy { DrawableResource("drawable:checkmarkAppLogo", setOf(ResourceItem(setOf(),"drawable/checkmarkAppLogo.png",-1,-1))) }
-internal val Res.drawable.compose_multiplatform: DrawableResource by lazy { DrawableResource("drawable:compose_multiplatform", setOf(ResourceItem(setOf(),"drawable/compose-multiplatform.xml",-1,-1))) }
-internal val Res.drawable.edit: DrawableResource by lazy { DrawableResource("drawable:edit", setOf(ResourceItem(setOf(),"drawable/edit.png",-1,-1))) }
+private const val MD: String = "composeResources/csci215final.composeapp.generated.resources/"
+
+@delegate:ResourceContentHash(-1_810_742_532)
+internal val Res.drawable.checkbox: DrawableResource by lazy {
+      DrawableResource("drawable:checkbox", setOf(
+        ResourceItem(setOf(), "${MD}drawable/checkbox.png", -1, -1),
+      ))
+    }
+
+@delegate:ResourceContentHash(11_821_879)
+internal val Res.drawable.checkmark: DrawableResource by lazy {
+      DrawableResource("drawable:checkmark", setOf(
+        ResourceItem(setOf(), "${MD}drawable/checkmark.png", -1, -1),
+      ))
+    }
+
+@delegate:ResourceContentHash(585_966_079)
+internal val Res.drawable.checkmarkAppLogo: DrawableResource by lazy {
+      DrawableResource("drawable:checkmarkAppLogo", setOf(
+        ResourceItem(setOf(), "${MD}drawable/checkmarkAppLogo.png", -1, -1),
+      ))
+    }
+
+@delegate:ResourceContentHash(379_089_144)
+internal val Res.drawable.compose_multiplatform: DrawableResource by lazy {
+      DrawableResource("drawable:compose_multiplatform", setOf(
+        ResourceItem(setOf(), "${MD}drawable/compose-multiplatform.xml", -1, -1),
+      ))
+    }
+
+@delegate:ResourceContentHash(314_667_795)
+internal val Res.drawable.edit: DrawableResource by lazy {
+      DrawableResource("drawable:edit", setOf(
+        ResourceItem(setOf(), "${MD}drawable/edit.png", -1, -1),
+      ))
+    }
+
+@InternalResourceApi
+internal fun _collectCommonMainDrawable0Resources(map: MutableMap<String, DrawableResource>) {
+  map.put("checkbox", Res.drawable.checkbox)
+  map.put("checkmark", Res.drawable.checkmark)
+  map.put("checkmarkAppLogo", Res.drawable.checkmarkAppLogo)
+  map.put("compose_multiplatform", Res.drawable.compose_multiplatform)
+  map.put("edit", Res.drawable.edit)
+}
