ed7ae2445de13982d0e41cbb532f47d1591e3487
grigolir <grigolir@moravian.edu>
2026-04-26 23:50:54 -0400

    app runs on Rachele end

---
+++ composeApp/src/androidMain/kotlin/com/example/csci215_final/MainApplication.kt.kt
@@ -0,0 +1,12 @@
+package com.example.csci215_final
+
+import android.app.Application
+import com.example.csci215_final.data.local.database.DatabaseFactory
+import com.example.csci215_final.di.ServiceLocator
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        ServiceLocator.init(DatabaseFactory(this).create())
+    }
+}
